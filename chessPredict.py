e3da388cc919f8b14789f7e7a7d2341c2fa13ccb
DanteRickheim <75857057+DanteRickheim@users.noreply.github.com>
2023-06-25 16:35:38 -0300

    Alternate solution, using MLPClassifier Uses multiple boardstates from each game Accuracy: ~50% with limited dataset

--- chessPredict.py
+++ chessPredict.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn
-import chess
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.utils import shuffle
 from google_drive_downloader import GoogleDriveDownloader as gdd
+import chess
 
 # Load data
 gdd.download_file_from_google_drive(file_id='1koNGmnBxvSzFM0dKDwgxzijjG2pFPKs5',
@@ -15,57 +15,110 @@ gdd.download_file_from_google_drive(file_id='1koNGmnBxvSzFM0dKDwgxzijjG2pFPKs5',
 df_Chess = pd.read_csv('data_chess.csv', sep = ",")
 
 # Drop columns
-df_Chess = df_Chess[['winner', 'moves']]
+df_Chess = df_Chess[['winner', 'moves', 'white_rating', 'black_rating']]
 df_Chess = df_Chess.dropna()
 
-# Use 5% of dataset only
-df_Chess = df_Chess.sample(frac=0.05)
+board = chess.Board()
 
 # Transform winner to 0, 1 or 2
 df_Chess['winner'] = df_Chess['winner'].map({'white': 0, 'black': 1, 'draw': 2})
 
-# Create board
-board = chess.Board()
-
 # Transform moves to array
 df_Chess['moves'] = df_Chess['moves'].apply(lambda x: x.split(' '))
 
-# Get 40% of moves
-df_Chess['moves'] = df_Chess['moves'].apply(lambda x: x[:int(len(x)*0.4)])
+#keep 90% of array
+df_Chess['moves'] = df_Chess['moves'].apply(lambda x: x[0:int(len(x)*0.9)])
 
-# Create board for each move
-def create_board(moves):
-    for i in range(len(moves)):
-        try:
-            board.push_san(moves[i])
-        except:
-            pass
-    return board.copy()
+#split df 80-20
+train = df_Chess[:int(len(df_Chess)*0.8)]
+test = df_Chess[int(len(df_Chess)*0.8):]
+test = test.reset_index(drop=True)
 
-df_Chess['moves'] = df_Chess['moves'].apply(lambda x: create_board(x))
+df_Chessboards_train = pd.DataFrame(columns=['winner', 'board'])
+df_Chessboards_test = pd.DataFrame(columns=['winner', 'board'])
 
-# Create new columns for each place on the board
-for i in range(64):
-    df_Chess['piece_' + str(i)] = df_Chess['moves'].apply(lambda x: x.piece_at(i).piece_type if x.piece_at(i) != None else 0)
+for i in range(len(train['moves'])):
+  for j in range(len(train['moves'][i])):
+    board.push_san(train['moves'][i][j])
+    if j % 4 == 0:
+      boardString = str(board).replace('\n', ' ')
+      df_Chessboards_train = df_Chessboards_train._append({'winner': train['winner'][i], 'board': boardString}, ignore_index=True)
+  print(i)
+  board.reset()
 
-# Split train and test using 20% of data for testing
-from sklearn.model_selection import train_test_split
+for i in range(len(test['moves'])):
+  for j in range(len(test['moves'][i])):
+    board.push_san(test['moves'][i][j])
+  boardString = str(board).replace('\n', ' ')
+  df_Chessboards_test = df_Chessboards_test._append({'winner': test['winner'][i], 'board': boardString}, ignore_index=True)
+  print(i)
+  board.reset()
 
-X = df_Chess.drop(['winner', 'moves'], axis=1)
-y = df_Chess['winner']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+#make column in df_Chess for each element in board
+df_Chessboards_train = df_Chessboards_train.join(df_Chessboards_train['board'].str.split(' ', expand=True).add_prefix('board'))
+df_Chessboards_test = df_Chessboards_test.join(df_Chessboards_test['board'].str.split(' ', expand=True).add_prefix('board'))
 
-# Train classifier model
-from sklearn.ensemble import RandomForestClassifier
+#drop board column
+df_Chessboards_train = df_Chessboards_train.drop(columns=['board'])
+df_Chessboards_test = df_Chessboards_test.drop(columns=['board'])
 
-model = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=0)
-model.fit(X_train, y_train)
+#replace letters of pieces with numbers
+df_Chessboards_train = df_Chessboards_train.replace('r', 1)
+df_Chessboards_train = df_Chessboards_train.replace('n', 2)
+df_Chessboards_train = df_Chessboards_train.replace('b', 3)
+df_Chessboards_train = df_Chessboards_train.replace('q', 4)
+df_Chessboards_train = df_Chessboards_train.replace('k', 5)
+df_Chessboards_train = df_Chessboards_train.replace('p', 6)
+df_Chessboards_train = df_Chessboards_train.replace('R', 7)
+df_Chessboards_train = df_Chessboards_train.replace('N', 8)
+df_Chessboards_train = df_Chessboards_train.replace('B', 9)
+df_Chessboards_train = df_Chessboards_train.replace('Q', 10)
+df_Chessboards_train = df_Chessboards_train.replace('K', 11)
+df_Chessboards_train = df_Chessboards_train.replace('P', 12)
+df_Chessboards_train = df_Chessboards_train.replace('.', 0)
+df_Chessboards_test = df_Chessboards_test.replace('r', 1)
+df_Chessboards_test = df_Chessboards_test.replace('n', 2)
+df_Chessboards_test = df_Chessboards_test.replace('b', 3)
+df_Chessboards_test = df_Chessboards_test.replace('q', 4)
+df_Chessboards_test = df_Chessboards_test.replace('k', 5)
+df_Chessboards_test = df_Chessboards_test.replace('p', 6)
+df_Chessboards_test = df_Chessboards_test.replace('R', 7)
+df_Chessboards_test = df_Chessboards_test.replace('N', 8)
+df_Chessboards_test = df_Chessboards_test.replace('B', 9)
+df_Chessboards_test = df_Chessboards_test.replace('Q', 10)
+df_Chessboards_test = df_Chessboards_test.replace('K', 11)
+df_Chessboards_test = df_Chessboards_test.replace('P', 12)
+df_Chessboards_test = df_Chessboards_test.replace('.', 0)
+print(df_Chessboards_train)
+print(df_Chessboards_test)
 
-# Predict
-y_pred = model.predict(X_test)
+#shuffle rows
+df_Chessboards_train = shuffle(df_Chessboards_train)
 
-# Evaluate
+#split df_Chessboards into x and y
+x_train = df_Chessboards_train.drop(columns=['winner'])
+y_train = df_Chessboards_train['winner']
+y_train = y_train.astype('int')
+
+x_test = df_Chessboards_test.drop(columns=['winner'])
+y_test = df_Chessboards_test['winner']
+y_test = y_test.astype('int')
+
+#train mlp model
+from sklearn.neural_network import MLPClassifier
+mlp = MLPClassifier(hidden_layer_sizes=(1000, 1000, 1000), max_iter=10000)
+mlp.fit(x_train, y_train)
+
+#predict
+y_pred = mlp.predict(x_test)
+
+#evaluate
 from sklearn.metrics import classification_report, confusion_matrix
-
+print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
+
+
+
+
+
